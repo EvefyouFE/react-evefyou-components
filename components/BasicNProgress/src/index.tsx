6caa724ad740a68f9346329114e0914e167f61ba
evef <eveforest@163.com>
2023-09-04 01:53:52 +0800

    add ngprocess css

--- components/BasicNProgress/src/index.tsx
+++ components/BasicNProgress/src/index.tsx
@@ -6,6 +6,7 @@
  * Everyone is coming to the world i live in, as i am going to the world lives for you. 人人皆往我世界，我为世界中人人。
  * Copyright (c) 2023 by EvefyouFE/evef, All Rights Reserved. 
  */
+import 'nprogress/nprogress.css';
 import nProgress from 'nprogress';
 import React, { PropsWithChildren, useEffect } from 'react';
 
